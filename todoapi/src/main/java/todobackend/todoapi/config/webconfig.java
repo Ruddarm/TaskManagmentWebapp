78277845de2141e9f23479da0eae95be2d6ca2c4
root <root@Ruddarm-lap>
2024-10-30 07:46:25 +0530

    making evn exeuctable

--- todoapi/src/main/java/todobackend/todoapi/config/webconfig.java
+++ todoapi/src/main/java/todobackend/todoapi/config/webconfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class webconfig implements WebMvcConfigurer{
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**") // Allow all paths
                 .allowedOrigins("http://localhost:3000") // Allow your frontend's origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
